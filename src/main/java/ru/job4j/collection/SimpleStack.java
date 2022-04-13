207bd6c9e3ccf91a9fbcb0f5153a88d049d1c4d5
PavelValger <pavelwalker@mail.ru>
2022-04-13 21:06:47 +0500

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -2,12 +2,19 @@ package ru.job4j.collection;
 
 public class SimpleStack<T> {
     private final ForwardLinked<T> linked = new ForwardLinked<>();
+    private int size = 0;
 
     public T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
