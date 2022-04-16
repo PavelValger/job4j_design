daeca60b6ab2a0cfa7c1dad784b750fd37e82afc
PavelValger <pavelwalker@mail.ru>
2022-04-16 11:02:17 +0500

    6. Перевернуть связанный список [#161 #289023] упростил метод revert()

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -46,8 +46,8 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public boolean revert() {
-        boolean rsl = false;
-        if (head != null && head.next != null) {
+        boolean rsl = head != null && head.next != null;
+        if (rsl) {
             Node<T> current = head.next;
             head.next = null;
             while (current != null) {
@@ -56,7 +56,6 @@ public class ForwardLinked<T> implements Iterable<T> {
                 head = current;
                 current = next;
             }
-            rsl = true;
         }
         return rsl;
     }
