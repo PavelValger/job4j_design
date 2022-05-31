4d07ab41ee953192db67be218360b683f7ae54a4
PavelValger <pavelwalker@mail.ru>
2022-05-31 10:03:01 +0300

    2. Анализ доступности сервера. [#859] Открыл оба потока в одном заголовке try

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,30 +5,25 @@ import java.io.*;
 public class Analizy {
 
     public void unavailable(String source, String target) {
-        try (PrintWriter record = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
+        try (PrintWriter record = new PrintWriter(new BufferedOutputStream(
+                new FileOutputStream(target)));
+             BufferedReader read = new BufferedReader(new FileReader(source))) {
             boolean statusOff = false;
             String start = null;
-            try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-                for (String line = read.readLine(); line != null; line = read.readLine()) {
-                    String[] divider = line.split(" ", 2);
-                    if (!statusOff && (line.startsWith("400") || line.startsWith("500"))) {
-                        statusOff = true;
-                        start = divider[1] + ";";
-                    } else if (!(line.startsWith("400") || line.startsWith("500"))) {
-                        statusOff = false;
-                        if (start != null) {
-                            record.printf("%s%n", start + divider[1] + ";");
-                        }
+            for (String line = read.readLine(); line != null; line = read.readLine()) {
+                String[] divider = line.split(" ", 2);
+                if (!statusOff && (line.startsWith("400") || line.startsWith("500"))) {
+                    statusOff = true;
+                    start = divider[1] + ";";
+                } else if (!(line.startsWith("400") || line.startsWith("500"))) {
+                    statusOff = false;
+                    if (start != null) {
+                        record.printf("%s%n", start + divider[1] + ";");
                     }
                 }
-                if (statusOff) {
-                    record.print(start.replace(";", "-"));
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            }
+            if (statusOff) {
+                record.print(start.replace(";", "-"));
             }
         } catch (IOException e) {
             e.printStackTrace();
