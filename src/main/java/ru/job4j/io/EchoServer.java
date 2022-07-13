c3fb9747dc137f3ef00bd875c09907771abbc695
PavelValger <pavelwalker@mail.ru>
2022-07-13 21:27:44 +0500

    3. Бот [#7921] доработал код на предмет PrintWriter

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,36 +1,40 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
+            int count = 1;
+            System.out.println("Server started");
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
+                try (PrintWriter out = new PrintWriter(
+                        new BufferedWriter(
+                                new OutputStreamWriter(socket.getOutputStream())));
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n");
                     String string = in.readLine();
-                    String[] divider = string.split(" ", 3);
-                    String[] message = divider[1].split("=", 2);
-                    if (!"Exit".equals(message[1])) {
-                        out.write(message[1].getBytes());
-                        for (String line = string;
-                             line != null && !line.isEmpty(); line = in.readLine()) {
-                            System.out.println(line);
+                    if (string.contains("=")) {
+                        String[] divider = string.split(" ", 3);
+                        String[] message = divider[1].split("=", 2);
+                        if (!"Exit".equals(message[1])) {
+                            System.out.printf("The user %s has connected%n", count++);
+                            out.write(String.format("%s\n", message[1]));
+                            for (String line = string;
+                                 line != null && !line.isEmpty(); line = in.readLine()) {
+                                System.out.println(line);
+                            }
+                        } else {
+                            out.write("Bye, dear friend.\n");
+                            server.close();
+                            System.out.println("The server has shut down");
                         }
-                    } else {
-                        out.write("Bye, dear friend.".getBytes());
-                        server.close();
-                        System.out.println("The server has shut down");
+                       out.flush();
                     }
-                    out.flush();
                 }
             }
         } catch (IOException e) {
