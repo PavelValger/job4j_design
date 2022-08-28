499979303747a8d90c84f32b0d4ebc2c91d10285
PavelValger <89793016+PavelValger@users.noreply.github.com>
2022-08-28 22:10:22 +0500

    1. Схема прав пользователи и роли. [#1093]

---
+++ PostgreSQL/init.sql
@@ -0,0 +1,13 @@
+-- Database: elements_system
+
+-- DROP DATABASE IF EXISTS elements_system;
+
+CREATE DATABASE elements_system
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
