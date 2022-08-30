cde00e7b9163d92a86345659076f71d339aa63ca
PavelValger <pavelwalker@mail.ru>
2022-08-30 20:01:05 +0500

    Обновление программы поиска дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,8 +1,11 @@
 package ru.job4j.io.duplicates;
 
+import java.io.Console;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class DuplicatesFinder {
 
@@ -16,8 +19,18 @@ public class DuplicatesFinder {
 
     public static void main(String[] args) {
         DuplicatesVisitor visitor = new DuplicatesVisitor();
+        System.out.println("Enter the search directory, for example \"D:\\Games\\\":");
+        Console console = System.console();
+        String directory;
+        if (console != null) {
+            directory = console.readLine();
+        } else {
+            var scanner = new Scanner(System.in);
+            directory = scanner.nextLine();
+        }
+        Path path = Path.of(directory).toAbsolutePath();
         try {
-            Files.walkFileTree(Paths.get("C:\\Users\\Павел\\Desktop\\music"),
+            Files.walkFileTree(Paths.get(String.valueOf(path)),
                     visitor);
         } catch (IOException e) {
             e.printStackTrace();
