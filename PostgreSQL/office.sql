85b6329bde211591e0b079089c424b09c8ea9d26
PavelValger <89793016+PavelValger@users.noreply.github.com>
2022-09-07 19:34:29 +0500

    0. Join [#6862 #340349] исправил ссылку references

--- PostgreSQL/office.sql
+++ PostgreSQL/office.sql
@@ -1,31 +1,31 @@
-create table employees(
+create table departments(
     id serial primary key,
 	name text
 );
 
-create table departments(
+create table employees(
     id serial primary key,
 	name text,
-	employee_id int REFERENCES employees(id)
+	department_id int REFERENCES departments(id)
 );
 
 
-insert into employees(name) VALUES ('Аня'), ('Ваня'), ('Боря'), ('Петя');
-INSERT into departments(name, employee_id) values ('HR', 1), ('ПО', 2),
-('ПО', 3), ('ПО', 4), ('Бережливое производство', null);
+INSERT into departments(name) values ('HR'), ('ПО'), ('Бережливое производство');
+insert into employees(name, department_id) values ('Аня', 1),
+('Ваня', 2), ('Боря', 2), ('Петя', 2), ('Регина', 1);
 
-select * from departments d left join employees e on d.employee_id = e.id;
-select * from departments d right join employees e on d.employee_id = e.id;
-select * from departments d full join employees e on d.employee_id = e.id;
+select * from departments d left join employees e on e.department_id = d.id;
+select * from departments d right join employees e on e.department_id = d.id;
+select * from departments d full join employees e on e.department_id = d.id;
 select * from departments d CROSS join employees e;
 
 select * from departments d left join employees e on d.employee_id = e.id
 where e.id is null;
 
-select d.name, d.employee_id, e.name
-from departments d left join employees e on d.employee_id = e.id;
-select d.name, d.employee_id, e.name
-from employees e right join departments d on d.employee_id = e.id;
+select d.name "Отдел", e.department_id, e.name "Сотрудник"
+from departments d left join employees e on e.department_id = d.id;
+select d.name "Отдел", e.department_id, e.name "Сотрудник"
+from employees e right join departments d on e.department_id = d.id;
 
 create table teens(
     id serial primary key,
@@ -36,6 +36,6 @@ create table teens(
 insert into teens(name, gender) VALUES ('Аня', 'Ж'), ('Ваня', 'М'),
 ('Боря', 'М'), ('Петя', 'М'), ('Алина', 'Ж');
 
-select distinct t1.name a, t2.name b, (t2.name, t1.name)
+select t1.name a, t2.name b, (t2.name, t1.name)
 as "Разнополая пара" from teens t1 cross join teens t2
-where t1.gender != t2.gender limit 6;
+where t1.gender != t2.gender and t1.gender = 'Ж';
