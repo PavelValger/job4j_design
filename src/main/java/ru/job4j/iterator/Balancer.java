22a570d2710355c4783f51bd56d6d4284f511660
Pavel Valger <pavelwalker@mail.ru>
2023-09-16 03:13:24 +0500

    7. Балансир [#505083] рефакторинг с индексом

--- src/main/java/ru/job4j/iterator/Balancer.java
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public class Balancer {
     public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
-        var listIterator = nodes.iterator();
-        while (source.hasNext() && listIterator.hasNext()) {
-            listIterator.next().add(source.next());
-            if (!listIterator.hasNext()) {
-                listIterator = nodes.iterator();
+        int index = 0;
+        while (source.hasNext() && index < nodes.size()) {
+            nodes.get(index++).add(source.next());
+            if (index == nodes.size()) {
+                index = 0;
             }
         }
     }
